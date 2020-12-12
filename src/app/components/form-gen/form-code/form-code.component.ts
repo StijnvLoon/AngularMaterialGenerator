3273a09b8c46fab885e03f25e016b1347533e4b7
Stijn van Loon <stijnvanloon@home.nl>
2020-12-12 14:38:58 +0100

    code improvements

--- src/app/components/form-gen/form-code/form-code.component.ts
+++ src/app/components/form-gen/form-code/form-code.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormTemplate } from 'src/app/models/formTemplate';
 import { FormTypeImport } from 'src/app/models/formType';
 import { PreviewFile } from 'src/app/models/previewFile';
+import { ImportsLibrary } from 'src/app/models/importsLibrary';
 
 @Component({
   selector: 'app-form-code',
@@ -15,25 +16,28 @@ export class FormCodeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  updatePreview(): void {
     this.previewFileList = this.getPreviewFiles()
   }
 
-  getPreviewFiles(): PreviewFile[] {
-
+  private getPreviewFiles(): PreviewFile[] {
     return [
-      this.getAppModuleFile()
+      this.getAppModuleFile(),
+      this.getComponentTSCode(),
+      this.getComponentHTMLCode()
     ]
   }
 
-  getAppModuleFile(): PreviewFile {
+  private getAppModuleFile(): PreviewFile {
     const appModuleFile: PreviewFile = new PreviewFile('app.module.ts')
     const imports: FormTypeImport[] = this.formTemplate.getImports()
 
     //optional imports
     appModuleFile.addToCodeLines(['...'])
     imports.forEach(formTypeImport => {
-      appModuleFile.addToCodeLines(['import { ' + formTypeImport.moduleName + ' } from \'' + formTypeImport.importLocation + ' \';'])
+      appModuleFile.addToCodeLines(['import { ' + formTypeImport.moduleName + ' } from \'' + formTypeImport.importLocation + '\';'])
     });
     appModuleFile.addToCodeLines(['...'])
 
@@ -41,15 +45,44 @@ export class FormCodeComponent implements OnInit {
     appModuleFile.addToCodeLines([
       '@NgModule({',
       '  imports: [',
-      '  ...'
+      '    ...'
     ])
     imports.forEach(formTypeImport => {
-      appModuleFile.addToCodeLines(['  ' + formTypeImport.moduleName])
+      appModuleFile.addToCodeLines(['    ' + formTypeImport.moduleName + ','])
     });
-    appModuleFile.addToCodeLines(['   ...', '  ]'])
+    appModuleFile.addToCodeLines(['    ...', '  ],'])
 
+    //providers
+    if(imports.includes(ImportsLibrary.MATDATEPICKERMODULE)) {
+      appModuleFile.addToCodeLines(['  providers: ['])
+      appModuleFile.addToCodeLines(['    ' + ImportsLibrary.MATDATEPICKERMODULE.moduleName + ','])
+      appModuleFile.addToCodeLines(['  ],'])
+    }
 
+    appModuleFile.addToCodeLines(['})', 'export class AppModule { }'])
 
     return appModuleFile
+  }
+
+  private getComponentTSCode(): PreviewFile {
+    const componentTSFile: PreviewFile = new PreviewFile(this.formTemplate.name.toLowerCase() + '.component.ts')
+
+    componentTSFile.addToCodeLines([
+      '@Component({',
+      "  selector: 'app-" + this.formTemplate.name.toLowerCase() + "',",
+      "  templateUrl: './" + this.formTemplate.name.toLowerCase() + ".component.html',",
+      "  styleUrls: ['./" + this.formTemplate.name.toLowerCase() + ".component.scss'],",
+      '})'
+    ])
+
+    return componentTSFile
+  }
+
+  private getComponentHTMLCode(): PreviewFile {
+    const componentHTMLFile: PreviewFile = new PreviewFile(this.formTemplate.name.toLowerCase() + '.component.html')
+
+    componentHTMLFile.addToCodeLines(['<p>test</p>'])
+
+    return componentHTMLFile
   }
 }
--- src/app/components/form-gen/form-gen.component.ts
+++ src/app/components/form-gen/form-gen.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormTypeKey } from 'src/app/models/enums/FormTypeKey';
 import { FormTemplate } from '../../models/formTemplate';
 import { FormTypeService } from '../../services/form-type.service';
+import { FormCodeComponent } from './form-code/form-code.component';
 
 @Component({
   selector: 'app-form-gen',
@@ -11,12 +12,19 @@ import { FormTypeService } from '../../services/form-type.service';
 export class FormGenComponent implements OnInit {
 
   public formTemplate: FormTemplate
+  @ViewChild(FormCodeComponent) private formCodeComponent: FormCodeComponent;
 
   constructor(private formTypeService: FormTypeService) { }
 
   ngOnInit(): void {
-    this.formTemplate = new FormTemplate('new template')
+    this.formTemplate = new FormTemplate('new_form')
     this.formTemplate.formTypeList.push(this.formTypeService.createFormType(FormTypeKey.INPUT_TEXT))
     this.formTemplate.formTypeList.push(this.formTypeService.createFormType(FormTypeKey.INPUT_PASSWORD))
+  }
+
+  onTabChanged($event) {
+    if($event.tab.textLabel == 'Code') {
+      this.formCodeComponent.updatePreview()
+    }
   }
 }
