a5eade48fa15c1cc56ac8175194a65837b7b6a04
Stijn van Loon <stijnvanloon@home.nl>
2021-02-07 20:27:29 +0100

    build fix

--- src/app/components/lobby/lobby.component.ts
+++ src/app/components/lobby/lobby.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { FormComponentLibrary } from 'dist/AMG/assets/formComponentLibrary';
-import { AddFormTypeDialog } from 'src/app/dialogs/addFormTypeDialog/add-formtype-dialog';
 import { SelectFormTemplateDialog } from 'src/app/dialogs/selectFormTemplateDialog/select-formtemplate-dialog';
-import { FormOptions } from 'src/app/models/FormOptions';
-import { FormSavable } from 'src/app/models/FormSavable';
 
 @Component({
   selector: 'app-lobby',
