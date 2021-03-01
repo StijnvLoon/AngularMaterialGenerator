2348f1dea83e4e66640db224e078418c8b61f609
Stijn van Loon <stijnvanloon@home.nl>
2021-03-01 19:20:47 +0100

    improved theming layout

--- src/app/modules/theme/components/theme-gen/theme-editor/palette-editor/palette-editor.component.ts
+++ src/app/modules/theme/components/theme-gen/theme-editor/palette-editor/palette-editor.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkDragMove } from '@angular/cdk/drag-drop';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { ThemePalette } from 'src/app/modules/theme/models/ThemePalette';
 import { ThemePaletteService } from 'src/app/modules/theme/services/ThemePalette.service';
 
@@ -19,5 +20,18 @@ export class PaletteEditorComponent implements OnInit {
       this.systemPalettes.push(this.themePaletteService.generatePallete(color.name, color.hex))
     })
   }
+
+  sortPredicate() {
+    return false
+  }
+
+  public onDragMove(event, id): void {
+    const nodeMovePreview = new ElementRef<HTMLElement>(document.getElementById(id));
+    const xPos = event.pointerPosition.x - 20
+    const yPos = event.pointerPosition.y - 20
+    if (nodeMovePreview?.nativeElement) {
+        nodeMovePreview.nativeElement.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`
+    }
+}
 
 }
