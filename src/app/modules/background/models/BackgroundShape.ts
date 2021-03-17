e0133dc656aed4c1cec97b4c9aa5d7d845abfb4e
Stijn van Loon <stijnvanloon@home.nl>
2021-03-17 08:35:45 +0100

    added background header buttons

--- src/app/modules/background/models/BackgroundShape.ts
+++ src/app/modules/background/models/BackgroundShape.ts
@@ -3,9 +3,9 @@ export class BackgroundShape {
     constructor(
         public shape: Shape = Shape.Rectangle,
         public name: string = 'black square',
-        public y: number = 0,
-        public x: number = 0,
-        public width: number = 50,
+        public y: number = 50,
+        public x: number = 50,
+        public width: number = 30,
         public height: number = 50,
         public color: string = '#000000',
         public shadowLvl: string = 'z0',
